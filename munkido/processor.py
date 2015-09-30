348a92b544ade24741d6aa22e1ac988878a98b93
Graham Pugh <g.r.pugh@gmail.com>
2015-09-30 19:25:57 -0400

    Attempt to obtain app_name in base.html

--- munkido/processor.py
+++ munkido/processor.py
@@ -14,9 +14,3 @@ def index(request):
 	return {
 			'webadmin_version': version}
 
-def resolver_context_processor(request):
-    return {
-        'app_name': request.resolver_match.app_name,
-        'namespace': request.resolver_match.namespace,
-        'url_name': request.resolver_match.url_name
-    }
