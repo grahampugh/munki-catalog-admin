8b8e9ed58f86b60e03fcfd86be27d5b988c411d7
Graham Pugh <g.r.pugh@gmail.com>
2015-10-09 16:47:44 -0400

    struggling with permissions

--- catalogs/urls.py
+++ catalogs/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('catalogs.views',
+    url(r'^test/*$', 'test'),
     url(r'^$', 'catalog_view'),
     url(r'^(?P<catalog_name>[^/]+)/$', 'catalog_view'),
     url(r'^(?P<catalog_name>[^/]+)/(?P<item_index>\d+)/$', 'item_detail'),
-	url(r'^test/*$', 'test'),
 )
