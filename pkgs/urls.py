8417927618d107d02742ad3aa6251708bd8b20a0
Graham Pugh <g.r.pugh@gmail.com>
2015-10-20 14:42:56 -0400

    Add orhaned page to urls.py

--- pkgs/urls.py
+++ pkgs/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('pkgs.views',
 	url(r'^index/*$', 'index'),
+	url(r'^orphaned/*$', 'orphaned'),
 	url(r'^confirm/*$', 'confirm'),
 	url(r'^done/*$', 'done'),
 	url(r'^deleted/*$', 'deleted'),
